48b431fb323aa9d580674ea08e304f4e527a1a5a
matthewbitbucket <199856sc@gmail.com>
2024-04-23 14:20:43 +0100

    Complete add hotel feature.

--- frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
+++ frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
@@ -19,7 +19,12 @@ export type HotelFormData = {
     childCount: number;
 };
 
-const ManageHotelForm = () => {
+type Props = {
+    onSave: (hotelFormData: FormData) => void;
+    isLoading: boolean;
+};
+
+const ManageHotelForm = ({ onSave, isLoading }: Props) => {
     const formMethods = useForm<HotelFormData>();
     const { handleSubmit } = formMethods;
 
@@ -38,7 +43,13 @@ const ManageHotelForm = () => {
 
         formDataJson.facilities.forEach((facility, index) => {
             formData.append(`facilities[${index}]`, facility);
-        })
+        });
+
+        Array.from(formDataJson.imageFiles).forEach((imageFile) => {
+            formData.append(`imageFiles`, imageFile);
+        });
+
+        onSave(formData);
     });
 
     return (
@@ -50,8 +61,12 @@ const ManageHotelForm = () => {
                 <GuestsSection />
                 <ImagesSection />
                 <span className="flex justify-end">
-                    <button type="submit" className="bg-blue-600 text-white p-2 font-bold hover:bg-blue-500 text-xl">
-                        Save
+                    <button
+                        disabled={isLoading}
+                        type="submit" 
+                        className="bg-blue-600 text-white p-2 font-bold hover:bg-blue-500 text-xl disabled:bg-gray-500"
+                    >
+                        {isLoading ? "Saving..." : "Save"}
                     </button>
                 </span>
             </form>
