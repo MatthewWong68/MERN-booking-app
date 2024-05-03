63f03ac5d53627305edc0d521922e3ae48df9f16
matthewbitbucket <199856sc@gmail.com>
2024-05-03 15:33:04 +0100

    Added search feature.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,6 +11,7 @@ import AddHotel from "./pages/AddHotel";
 import { useAppContext } from "./contexts/AppContext";
 import MyHotels from "./pages/MyHotels";
 import EditHotel from "./pages/EditHotel";
+import Search from "./pages/Search";
 
 const App = () => {
   const { isLoggedIn } = useAppContext();
@@ -29,9 +30,9 @@ const App = () => {
           path="/search" 
           element={
             <Layout>
-              <p>Search Page</p>
+              <Search />
             </Layout>
-          } 
+          }
         />
         <Route 
           path="/register" 
--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -1,9 +1,10 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header"
 import Hero from "../components/Hero";
+import SearchBar from "../components/SearchBar";
 
 interface Props {
-    children: React.ReactNode
+    children: React.ReactNode;
 }
 
 const Layout = ({children}: Props) => {
@@ -11,12 +12,15 @@ const Layout = ({children}: Props) => {
         <div className="flex flex-col min-h-screen">
             <Header />
             <Hero />
+            <div className="container mx-auto">
+                <SearchBar />
+            </div>
             <div className="container mx-auto py-10 flex-1">
                 {children}
             </div>
             <Footer />
         </div>
-    )
-}
+    );
+};
 
 export default Layout;
